dfd217dc64c4cb5e31b4e4d24cf4f13853eca470
Dmitry <d3kon@mail.ru>
2023-06-08 08:51:12 +0800

    Добавил 3 дз

---
+++ src/main/java/org/example/home1/task1/data/Animal.java
@@ -0,0 +1,47 @@
+package org.example.home1.task1.data;
+
+import org.example.home1.task1.Ordered;
+
+public abstract class Animal implements Ordered {
+
+    private String name;
+
+    private int age;
+
+    public Animal() {
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int getOrder() {
+        return LOWEST_PRECEDENCE;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+}
